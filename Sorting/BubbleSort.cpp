e547492efb75112d76533a56387da2b05458c482
SUNIL SINGH <97845210+SS12220@users.noreply.github.com>
2025-03-17 23:16:25 +0530

    Added bubble sort.

---
+++ Sorting/BubbleSort.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+// g++ BubbleSort.cpp -o output.exe; .\output.exe  
+
+void bubbleSort(vector<int> &ar, int n){
+
+  for (size_t i = 0; i < n; i++)
+  {
+    for (size_t j = 0; j < n - i - 1; j++)
+    {
+      if (ar[j] > ar[j+1])
+      {
+        swap(ar[j], ar[j+1]);
+      }
+      
+    }
+    
+  }
+  
+}
+
+int main() {
+    vector<int> arr = {4, 1, 5 ,2, 3};
+    int n=5;
+
+    bubbleSort(arr, n);
+
+    for (size_t i = 0; i < n; i++)
+    {
+      cout << arr[i] << endl;
+    }
+    
+
+    return 0;
+}
