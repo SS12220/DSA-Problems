29d69e153c10fbcc6de41f6b6fc2a7075bd136b2
SUNIL SINGH <97845210+SS12220@users.noreply.github.com>
2025-03-17 23:26:30 +0530

    Added print array function.

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,11 +5,18 @@ using namespace std;
 
 // g++ BubbleSort.cpp -o output.exe; .\output.exe  
 
-void bubbleSort(vector<int> ar, int n){
-
-  for (size_t i = 0; i < n; i++)
+void printArray(vector<int> ar){
+  for (size_t i = 0; i < ar.size(); i++)
   {
-    for (size_t j = 0; j < n - i - 1; j++)
+    cout << ar[i] << endl;
+  }
+}
+
+void bubbleSort(vector<int> ar, int n){ //O(n^2)
+
+  for (size_t i = 0; i < n; i++) // if run n times
+  {
+    for (size_t j = 0; j < n - i - 1; j++) // then this will run n * n times
     {
       if (ar[j] > ar[j+1])
       {
@@ -17,21 +24,16 @@ void bubbleSort(vector<int> ar, int n){
       }
       
     }
-    
-  }
-
-  for (size_t i = 0; i < n; i++)
-  {
-    cout << ar[i] << endl;
   }
   
+  printArray(ar);
 }
 
 int main() {
     vector<int> arr = {4, 1, 5 ,2, 3};
     int n=5;
 
-    bubbleSort(arr, n);
+    bubbleSort(arr, n); // TC - O(n^2)
     
     return 0;
 }
