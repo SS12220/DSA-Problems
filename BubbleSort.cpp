31cfcc4bfecbad265a2328acb5b5e04cbcb78fd6
SUNIL SINGH <97845210+SS12220@users.noreply.github.com>
2025-03-18 10:05:30 +0530

    updated bubble sort and added sample file.

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -5,28 +5,33 @@ using namespace std;
 
 // g++ BubbleSort.cpp -o output.exe; .\output.exe  
 
-void printArray(vector<int> ar){
-  for (size_t i = 0; i < ar.size(); i++)
-  {
-    cout << ar[i] << endl;
-  }
-}
-
-void bubbleSort(vector<int> ar, int n){ //O(n^2)
+void bubbleSort(vector<int> &ar, int n){ //O(n^2)
 
   for (size_t i = 0; i < n; i++) // if run n times
   {
+    bool isSwaped = false;
     for (size_t j = 0; j < n - i - 1; j++) // then this will run n * n times
     {
       if (ar[j] > ar[j+1])
       {
+        isSwaped = true;
         swap(ar[j], ar[j+1]);
       }
       
     }
+
+    if (!isSwaped) return; // array is already sorted
+    
   }
-  
-  printArray(ar);
+}
+
+void printArray(vector<int> ar){
+  for (size_t i = 0; i < ar.size(); i++)
+  {
+    cout << ar[i] << " ";
+  }
+
+  cout << endl;
 }
 
 int main() {
@@ -34,6 +39,7 @@ int main() {
     int n=5;
 
     bubbleSort(arr, n); // TC - O(n^2)
+    printArray(arr);
     
     return 0;
 }
---
+++ SampleFile.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//#include <vector>
+//#include <climits>
+
+// g++ SampleFile.cpp -o output.exe; .\output.exe  
+
+int main() {    
+    return 0;
+}
